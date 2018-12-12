5659d051f4ecbe1183e3620a138554404793d2d9
Evan Pavley <epavley@sva.edu>
2018-12-12 14:43:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
-
+class CashRegister
+  attr_accessor :total, :discount
+  
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+  end
+  
+  def total
+    @total = @total
+  end
+  
+  def add_item(title, price, *quantity)
+    quantity = quantity
+    @total += price
+  end
+  
+  def apply_discount
+    
+  end
+  
+  def items
+    
+  end
+  
+  def void_last_transaction
+    
+  end
+  
+end
